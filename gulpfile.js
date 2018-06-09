3bc30967d67343b6a502cd9c7f7e59dbcfb70191
Abhinav <abhinavjha98ald@gmail.com>
2018-06-09 14:46:03 +0530

    Added the postcss and autoprefixer file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer =require('autoprefixer'),
+cssvars =require('postcss-simple-vars'),
+nested = require('postcss-nested');
 gulp.task('default',function(){
 	console.log("HO");
 });
@@ -9,7 +13,9 @@ gulp.task('html',function(){
 });
 
 gulp.task('styles',function(){
-	console.log("why so man in styles");
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssvars,nested,autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch',function(){
